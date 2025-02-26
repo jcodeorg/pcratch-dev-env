16b45756bd6ca52bb598c3dfcef563bc5e2279d7
Koichi Ito <kito@j-code.org>
2025-02-26 12:19:27 +0900

    m8

--- docs/microbitMore.mjs
+++ docs/microbitMore.mjs
@@ -4225,12 +4225,12 @@ var MicrobitMore = /*#__PURE__*/function () {
       var _this7 = this;
       var data = uint8ArrayToBase64(new Uint8Array([command.id].concat(_toConsumableArray(command.message))));
       return new Promise(function (resolve) {
-        console.log('sendCommand7', command.id, command.message);
+        console.log('sendCommand8', command.id, command.message);
         _this7._ble.write(MM_SERVICE.ID, MM_SERVICE.COMMAND_CH, data, 'base64', false // true // resolve after peripheral's response. // false
         );
         setTimeout(function () {
           return resolve();
-        }, 2000);
+        }, 1000);
         // setTimeout(() => resolve(), this.sendCommandInterval);
       });
     }
